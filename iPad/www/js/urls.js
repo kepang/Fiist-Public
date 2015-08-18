f10a477675edf878b5e2fd0f437a7e34a32ba355
kp <kepang.zz@gmail.com>
2015-08-17 17:45:21 -0700

    Initial upload: Manager client (iPad)

---
+++ iPad/www/js/urls.js
@@ -0,0 +1,5 @@
+var FIIST_GET_BUSINESS_URL = 'http://fiistserver.herokuapp.com/businesses.json';
+var FIIST_USER_REGISTRATION_URL = 'http://fiistserver.herokuapp.com/users.json';
+var FIIST_USER_SIGNIN_URL = 'http://fiistserver.herokuapp.com/users/sign_in.json';
+var FIIST_WAITLIST_URL = 'http://fiistserver.herokuapp.com/waitlists.json';
+var FIIST_NODEJS_URL = 'http://fiistnodejs.herokuapp.com'
